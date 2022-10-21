cf2b14764d1ef23054098dd143c4ae5d275a7b52
willi6m7 <willi6m7@mail.uc.edu>
2022-10-20 23:51:24 -0400

    change loading addresses of spritesheets

--- game_folder/Scene1.js
+++ game_folder/Scene1.js
@@ -4,37 +4,37 @@ class Scene1 extends Phaser.Scene{
     }
 
     preload(){
-        this.load.image("background", "assets/images/background.png");
-        this.load.spritesheet("ship", "assets/spritesheets/ship.png",{
+        this.load.image("background", "game_folder/assets/images/background.png");
+        this.load.spritesheet("ship", "game_folder/assets/spritesheets/ship.png",{
             frameWidth: 16,
             frameHeight: 16
         });
-        this.load.spritesheet("ship2", "assets/spritesheets/ship2.png",{
+        this.load.spritesheet("ship2", "game_folder/assets/spritesheets/ship2.png",{
             frameWidth: 32,
             frameHeight: 16
         });
-        this.load.spritesheet("ship3", "assets/spritesheets/ship3.png",{
+        this.load.spritesheet("ship3", "game_folder/assets/spritesheets/ship3.png",{
             frameWidth: 32,
             frameHeight: 32
         });
-        this.load.spritesheet("explosion", "assets/spritesheets/explosion.png",{
+        this.load.spritesheet("explosion", "game_folder/assets/spritesheets/explosion.png",{
             frameWidth: 16,
             frameHeight: 16
         });
-        this.load.spritesheet("power-up", "assets/spritesheets/power-up.png",{
+        this.load.spritesheet("power-up", "game_folder/assets/spritesheets/power-up.png",{
             frameWidth: 16,
             frameHeight: 16
         });
-        this.load.spritesheet("player", "assets/spritesheets/player.png",{
+        this.load.spritesheet("player", "game_folder/assets/spritesheets/player.png",{
             frameWidth: 16,
             frameHeight: 24
         });
-        this.load.spritesheet("beam", "assets/spritesheets/beam.png",{
+        this.load.spritesheet("beam", "game_folder/assets/spritesheets/beam.png",{
             frameWidth: 16,
             frameHeight: 16
         });
 
-        this.load.bitmapFont("pixelFont", "assets/font/font.png", "assets/font/font.xml");
+        this.load.bitmapFont("pixelFont", "game_folder/assets/font/font.png", "assets/font/font.xml");
     }
 
     create(){
